65e3b06d159b4811f49e6d4be87c20c4f6a25255
wimpykatana <wim.katana@gmail.com>
2018-06-07 14:40:07 +0700

    menu with icon

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
 
 import Explore from './screens/Explore';
@@ -8,32 +8,64 @@ import Inbox from './screens/Inbox';
 import Profile from './screens/Profile';
 import Upload from './screens/Upload';
 
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
 export default createBottomTabNavigator ({
     Explore:{
-        screen: Explore
+        screen: Explore,
+        navigationOptions:{
+            tabBarLabel: 'CARI',
+            tabBarIcon:({tintColor}) => (
+                <Image source={require('./assets/menuIcon/exploreIcon.png')}
+                       style={{ height: 23, width: 28, tintColor: tintColor }}/>
+            )
+        }
     },
     Inbox:{
-        screen: Inbox
+        screen: Inbox,
+        navigationOptions:{
+            tabBarLabel: 'PESAN',
+            tabBarIcon:({tintColor}) => (
+                <Image source={require('./assets/menuIcon/inboxIcon.png')}
+                       style={{ height: 24, width: 32, tintColor: tintColor }}/>
+            )
+        }
     },
     Upload:{
-        screen: Upload
+        screen: Upload,
+        navigationOptions:{
+            tabBarLabel: 'UPLOAD',
+            tabBarIcon:({tintColor}) => (
+                <Image source={require('./assets/menuIcon/uploadIcon.png')}
+                       style={{ height: 22, width: 22, tintColor: tintColor }}/>
+            )
+        }
     },
     Favorite:{
-        screen: Favorite
+        screen: Favorite,
+        navigationOptions:{
+            tabBarLabel: 'FAVORITE',
+            tabBarIcon:({tintColor}) => (
+                <Image source={require('./assets/menuIcon/favoriteIcon.png')}
+                       style={{ height: 22, width: 25, tintColor: tintColor }}/>
+            )
+        }
     },
     Profile:{
-        screen: Profile
+        screen: Profile,
+        navigationOptions:{
+            tabBarLabel: 'AKUN SAYA',
+            tabBarIcon:({tintColor}) => (
+                <Image source={require('./assets/menuIcon/profileIcon.png')}
+                       style={{ height: 23, width: 21, tintColor: tintColor }}/>
+            )
+        }
     }
 })
+
+const styles = StyleSheet.create({
+    iconExplore: {
+        height: 25,
+        width: 31
+    },
+});
 
 
