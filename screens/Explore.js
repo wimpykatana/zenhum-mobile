312b4d57ecc627671657b0bc1cdff216f47a275f
wimpykatana <wim.katana@gmail.com>
2018-06-20 12:51:26 +0700

    explore change to map view

--- screens/Explore.js
+++ screens/Explore.js
@@ -9,18 +9,68 @@ import {
     Platform,
     StatusBar,
     ScrollView,
+    Dimensions,
     Image
 } from 'react-native';
+import { Constants, MapView, Location, Permissions } from 'expo';
 
-import Slide from "../component/__smalComponent/Slide"
-import { Font } from 'expo';
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 
 class Explore extends Component {
 
-    state = {
-        fontLoaded: false,
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            fontLoaded: false,
+            mapRegion: null ,
+            hasLocationPermissions: false,
+            locationResult: null
+
+        };
+    }
+
+    componentDidMount() {
+        this._getLocationAsync();
+    }
+
+
+
+    _handleMapRegionChange = mapRegion => {
+        // console.log(mapRegion);
+        this.setState({ mapRegion });
     };
+
+    //get location
+    _getLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({
+                locationResult: 'Permission to access location was denied',
+            });
+        } else {
+            this.setState({ hasLocationPermissions: true });
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        this.setState({ locationResult: JSON.stringify(location) });
+
+        // Center the map on the location we just fetched.
+        this.setState({mapRegion: {
+                            latitude: location.coords.latitude,
+                            longitude: location.coords.longitude,
+                            latitudeDelta: 0.0922, longitudeDelta: LONGITUDE_DELTA }});
+
+        // console.log(this.state.mapRegion.latitude +" "+ this.state.mapRegion.latitude)
+    };
+
 
     componentWillMount(){
 
@@ -30,22 +80,12 @@ class Explore extends Component {
         }
     }
 
-    async componentDidMount(){
-        await Font.loadAsync({
-            'catamaran-regular': require('../assets/fonts/Catamaran/Catamaran-Regular.ttf'),
-            'catamaran-medium': require('../assets/fonts/Catamaran/Catamaran-Medium.ttf'),
-            'catamaran-bold': require('../assets/fonts/Catamaran/Catamaran-Bold.ttf')
-        });
-
-        this.setState({ fontLoaded: true });
-    }
-
 
 
     render() {
         return (
             <SafeAreaView style={{ flex: 1 }}>
-                <View style={{flex: 1}}>
+                <View style={styles.container}>
 
                     {/*search holder*/}
                     <View style={{ height: this.startHeaderHeight, borderBottomWidth: 1, borderBottomColor: '#ddd', paddingTop:10 }}>
@@ -70,28 +110,48 @@ class Explore extends Component {
                         </View>
                     </View>
 
-                    {/*scroll view or content*/}
-                    <ScrollView scrollEventThrottle = {16} >
 
-                        {
-                            this.state.fontLoaded ? (
-                                <View style={styles.container}>
-                                    <Text style={{ fontFamily: 'catamaran-bold', fontSize: 18, marginHorizontal:10 }}>
-                                        Rekomendasi unit
-                                    </Text>
-                                </View>
-                            ) : null
-                        }
+                    {
+                        this.state.locationResult === null ?
+                            <Text>Finding your current location...</Text> :
+                            this.state.hasLocationPermissions === false ?
+                                <Text>Location permissions are not granted.</Text> :
+                                this.state.mapRegion === null ?
+                                    <Text>Map region doesn't exist.</Text> :
+                                    <MapView
+                                        style={{ flex: 1 }}
+                                        initialRegion={this.state.mapRegion}
+                                        onRegionChange={this._handleMapRegionChange}
+                                    >
+                                        <MapView.Marker
+                                            draggable
+                                            coordinate={
+                                                {
+                                                    "latitude": -6.192764,
+                                                    "latitudeDelta": 0.09219999999824324,
+                                                    "longitude": 106.655359,
+                                                    "longitudeDelta": 0.07365126231377417,
+                                                }
+                                            }
+                                            title="Rumah Ini Di Jual"
+                                            description="Harga 20juta"
+                                        />
 
-                        <View style={{ height:120, marginTop:10, marginLeft:5 }}>
-                            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                                <Slide/>
-                                <Slide/>
-                                <Slide/>
-                            </ScrollView>
-                        </View>
-                    </ScrollView>
-
+                                        <MapView.Marker
+                                            draggable
+                                            coordinate={
+                                                {
+                                                    "latitude": -6.193671,
+                                                    "latitudeDelta": 0.09219999999824324,
+                                                    "longitude": 106.652666,
+                                                    "longitudeDelta": 0.07365126231377417,
+                                                }
+                                            }
+                                            title="Rumah Ini Di jual"
+                                            description="Harga 10juta"
+                                        />
+                                    </MapView>
+                    }
                 </View>
             </SafeAreaView>
         );
@@ -103,7 +163,6 @@ export default Explore;
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        paddingTop: 20,
         backgroundColor: '#fff'
     }
 });
